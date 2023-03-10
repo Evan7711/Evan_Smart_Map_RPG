bcc3d20e17170f9d7675ec581ce74a02f4b71d1e
Evan Smart <124279899+Evan7711@users.noreply.github.com>
2023-03-10 15:26:19 +0000

    Added Header, and Map spaces

---
+++ main.py
@@ -0,0 +1,14 @@
+#------------------------------------------------------------------------------
+# Created By: Evan Smart
+# Created On: 03/10/2023
+# Version: 1.0
+# Class: CS 30
+# Assignment: Map RPG
+
+Ship_map = [
+  ["Pirate Tile", "NothingTile", "StorageTile", "EscapeTile"],
+  ["NothingTile", "StorageTile", "DeckTile", "WheelHouseTile"],
+  ["StartTile", "PirateTile", "RoomTile", "StorageTile"],
+  ["BoilerTile", "NothingTile", "PirateTile", "NothingTile"]
+]
+print(Ship_map)
